c24c6538ea17535d42f0444b6fa0364a20882b1c
13568810905 <weiyi@zhengweiyi.cn>
2018-04-21 14:12:19 +0800

    创建库

---
+++ js/statistics.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b7cc9605f089a66ec65f9ccaafc223a1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
